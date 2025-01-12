c04cce695455d70e74a3f176037cf3b4fc9377ae
Mahmoud Alsoudy <113215972+Mahmoudshar0@users.noreply.github.com>
2025-01-12 17:07:13 +0200

    update

--- js/home.js
+++ js/home.js
@@ -6,3 +6,25 @@ function showMenu(){
 function hideMenu(){
    menu.style.right = "-300px";
 }
+
+// arrow icon
+let arrow = document.getElementById("arrow");
+window.addEventListener('scroll', () => {
+   if (window.scrollY > 200) {
+      arrow.style.opacity = "1";
+   } else {
+      arrow.style.opacity = "0";
+   }
+});
+
+
+// function updateScrollPosition() {
+//    console.log(window.scrollX);
+//    console.log(window.scrollY);
+// }
+
+// // Event listener for scroll
+// window.addEventListener('scroll', updateScrollPosition);
+
+// // Initial display
+// updateScrollPosition();
