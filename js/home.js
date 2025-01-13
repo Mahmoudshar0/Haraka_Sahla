34763c18f777596777c9596a952fdcf0e4ee241c
Mahmoud Alsoudy <113215972+Mahmoudshar0@users.noreply.github.com>
2025-01-13 11:58:13 +0200

    update

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 // Nav Bar
 let menu = document.getElementById("menu");
 function showMenu(){
-   menu.style.right = "0";
+   menu.style.right = "-5px";
 }
 function hideMenu(){
    menu.style.right = "-300px";
@@ -18,13 +18,14 @@ window.addEventListener('scroll', () => {
 });
 
 
-// function updateScrollPosition() {
-//    console.log(window.scrollX);
-//    console.log(window.scrollY);
-// }
-
-// // Event listener for scroll
-// window.addEventListener('scroll', updateScrollPosition);
-
-// // Initial display
-// updateScrollPosition();
+// slider
+function slider() {
+   let slider = document.getElementById("x");
+   let sliderImages = ["/images/chat1.webp","/images/forest.jpg","/images/forest2.jpg","/images/home_1.png"];
+   let i = 0;
+   setInterval(() => {
+      slider.style.backgroundImage = `url(${sliderImages[i]})`;
+      i = (i + 1) % sliderImages.length;
+   }, 3000);
+}
+slider();
