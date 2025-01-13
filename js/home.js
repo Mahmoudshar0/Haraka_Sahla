4f514d159a00d2e453c894d611161375a1dcb1df
Mahmoud Alsoudy <113215972+Mahmoudshar0@users.noreply.github.com>
2025-01-13 21:14:56 +0200

    finish home page

--- js/home.js
+++ js/home.js
@@ -9,11 +9,14 @@ function hideMenu(){
 
 // arrow icon
 let arrow = document.getElementById("arrow");
+let wts = document.getElementById("wts");
 window.addEventListener('scroll', () => {
    if (window.scrollY > 200) {
+      wts.style.bottom = "70px";
       arrow.style.opacity = "1";
    } else {
       arrow.style.opacity = "0";
+      wts.style.bottom = "130px";
    }
 });
 
